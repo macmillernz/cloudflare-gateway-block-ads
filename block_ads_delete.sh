d64a28147e27576e337cc933749723d2470a1a45
Jacob Gelling <jacobgelling@users.noreply.github.com>
2023-07-22 01:44:26 +0200

    Initial commit

---
+++ block_ads_delete.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Replace these variables with your actual Cloudflare API token and account ID
+API_TOKEN=${{ secrets.API_TOKEN }}
+ACCOUNT_ID=${{ secrets.ACCOUNT_ID }}
+PREFIX="Block ads"
+
+# Define error function
+function error() {
+    echo "Error: $1"
+}
+
+# Get current lists from Cloudflare
+current_lists=$(curl -sSfL -X GET "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/lists" \
+    -H "Authorization: Bearer ${API_TOKEN}" \
+    -H "Content-Type: application/json") || error "Failed to get current lists from Cloudflare"
+    
+# Get current policies from Cloudflare
+current_policies=$(curl -sSfL -X GET "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules" \
+    -H "Authorization: Bearer ${API_TOKEN}" \
+    -H "Content-Type: application/json") || error "Failed to get current policies from Cloudflare"
+
+# Delete policy with $PREFIX as name
+echo "Deleting policy..."
+policy_id=$(echo "${current_policies}" | jq -r --arg PREFIX "${PREFIX}" '.result | map(select(.name == $PREFIX)) | .[0].id') || error "Failed to get policy ID"
+curl -sSfL -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules/${policy_id}" \
+    -H "Authorization: Bearer ${API_TOKEN}" \
+    -H "Content-Type: application/json" > /dev/null || error "Failed to delete policy"
+
+# Delete all lists with $PREFIX in name
+echo "Deleting lists..."
+for list_id in $(echo "${current_lists}" | jq -r --arg PREFIX "${PREFIX}" '.result | map(select(.name | contains($PREFIX))) | .[].id'); do
+    echo "Deleting list ${list_id}..."
+    curl -sSfL -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/lists/${list_id}" \
+        -H "Authorization: Bearer ${API_TOKEN}" \
+        -H "Content-Type: application/json" > /dev/null || error "Failed to delete list ${list_id}"
+done
