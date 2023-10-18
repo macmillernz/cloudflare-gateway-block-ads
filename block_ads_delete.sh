56a639768b4475b1e5347ae76fe8af2cc7b66054
Jacob Gelling <3254275+jacobgelling@users.noreply.github.com>
2023-10-19 00:21:58 +0200

    Auto retry failed curl requests

--- block_ads_delete.sh
+++ block_ads_delete.sh
@@ -4,6 +4,7 @@
 API_TOKEN="$API_TOKEN"
 ACCOUNT_ID="$ACCOUNT_ID"
 PREFIX="Block ads"
+MAX_RETRIES=5
 
 # Define error function
 function error() {
@@ -16,19 +17,19 @@ rm oisd_small_domainswild2.txt
 rm oisd_small_domainswild2.txt.*
 
 # Get current lists from Cloudflare
-current_lists=$(curl -sSfL -X GET "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/lists" \
+current_lists=$(curl -sSfL --retry "$MAX_RETRIES" -X GET "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/lists" \
     -H "Authorization: Bearer ${API_TOKEN}" \
     -H "Content-Type: application/json") || error "Failed to get current lists from Cloudflare"
     
 # Get current policies from Cloudflare
-current_policies=$(curl -sSfL -X GET "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules" \
+current_policies=$(curl -sSfL --retry "$MAX_RETRIES" -X GET "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules" \
     -H "Authorization: Bearer ${API_TOKEN}" \
     -H "Content-Type: application/json") || error "Failed to get current policies from Cloudflare"
 
 # Delete policy with $PREFIX as name
 echo "Deleting policy..."
 policy_id=$(echo "${current_policies}" | jq -r --arg PREFIX "${PREFIX}" '.result | map(select(.name == $PREFIX)) | .[0].id') || error "Failed to get policy ID"
-curl -sSfL -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules/${policy_id}" \
+curl -sSfL --retry "$MAX_RETRIES" -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/rules/${policy_id}" \
     -H "Authorization: Bearer ${API_TOKEN}" \
     -H "Content-Type: application/json" > /dev/null || error "Failed to delete policy"
 
@@ -36,7 +37,7 @@ curl -sSfL -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID
 echo "Deleting lists..."
 for list_id in $(echo "${current_lists}" | jq -r --arg PREFIX "${PREFIX}" '.result | map(select(.name | contains($PREFIX))) | .[].id'); do
     echo "Deleting list ${list_id}..."
-    curl -sSfL -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/lists/${list_id}" \
+    curl -sSfL --retry "$MAX_RETRIES" -X DELETE "https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/gateway/lists/${list_id}" \
         -H "Authorization: Bearer ${API_TOKEN}" \
         -H "Content-Type: application/json" > /dev/null || error "Failed to delete list ${list_id}"
 done
